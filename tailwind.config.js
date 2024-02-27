5ba2bf296a218a3a3e313ecb4240bf23ddc26200
Treasure <odetokuntreasure6@gmail.com>
2024-02-27 22:02:59 +0100

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {},
+    fontFamily: {
+      sans: ["Kumbh Sans", "sans-serif;"],
+    },
+  },
+  plugins: [],
+};
