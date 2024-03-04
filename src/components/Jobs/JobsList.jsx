4572539198d82d6e9a6d53b31c2479d83b4c15a3
Treasure <odetokuntreasure6@gmail.com>
2024-03-05 00:46:55 +0100

    new changes

--- src/components/Jobs/JobsList.jsx
+++ src/components/Jobs/JobsList.jsx
@@ -3,10 +3,12 @@ import List from "../List/List";
 /* eslint-disable react/prop-types */
 function JobsList({ job }) {
   return (
-    <div className="flex flex-row flex-wrap items-center ml-[5rem] mt-[10rem] gap-10">
-      {job.map((aJob, index) => (
-        <List aJob={aJob} key={index} />
-      ))}
+    <div className="bg-[#F4F6F8]">
+      <div className="grid grid-cols-3 gap-5 items-center justify-center pt-20 pb-20 pl-14 pr-14">
+        {job.map((aJob, index) => (
+          <List aJob={aJob} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,16 +1,24 @@
 /* eslint-disable react/prop-types */
 function List({ aJob }) {
+  const { logoBackground } = aJob;
+  console.log(`bg-[${logoBackground}]`);
   return (
-    <div className="bg-red-100">
-      <div className="">
-        <ul>
+    <div className="bg-white col-span-1 rounded-md relative mb-10">
+      <div
+        className={`bg-[${logoBackground}]   w-[50px] h-[50px] absolute top-[-10%] flex items-center justify-center rounded-lg`}
+      >
+        <img src={aJob.logo} alt={aJob.company} />
+      </div>
+
+      <div className="m-5">
+        <div className="flex flex-row">
           <li>{aJob.postedAt}</li>
           <li>{aJob.contract}</li>
-        </ul>
+        </div>
+        <div className="text-xl">{aJob.position}</div>
+        <div className="text-[18px]">{aJob.company}</div>
+        <div className="text-[16px] text-[#5964E0]">{aJob.location}</div>
       </div>
-      <div className="">{aJob.position}</div>
-      <div className="">{aJob.company}</div>
-      <div className="">{aJob.location}</div>
     </div>
   );
 }
